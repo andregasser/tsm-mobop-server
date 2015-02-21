c160cc381435d0dbd12205b47a9c4c256862bced
André Gasser <andre.gasser@ispin.ch>
2015-02-21 18:06:24 +0100

    Initial version of JSON Web API.

---
+++ TsmMobOpService/Models/PointOfInterest.cs
@@ -0,0 +1,21 @@
+﻿namespace TsmMobOpService.Models
+{
+    public class PointOfInterest
+    {
+        public PointOfInterest(int id, string name, string description, int position)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Description = description;
+            this.Position = position;
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int Position { get; set; }
+    }
+}
